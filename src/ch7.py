b413095d3b6094ae917639e4b9890c4abb2f8bd4
kylerisse <kylerisse@users.noreply.github.com>
2020-11-30 16:57:23 -0800

    more chapter 7

--- src/ch7.py
+++ src/ch7.py
@@ -2,6 +2,8 @@
 Chapter 7
 '''
 
+import ch2
+
 
 def join_numbers(val):
     '''
@@ -17,15 +19,80 @@ def sum_numbers(word):
     return sum(int(x) for x in word.split() if x.isdigit())
 
 
-def flatten(items):
+def flatten_recursive(items):
     '''
     flatten list(s), recursive with state
     '''
     ret = []
     for item in items:
         if isinstance(item, list):
-            for sub_item in flatten(item):
+            for sub_item in flatten_recursive(item):
                 ret.append(sub_item)
         else:
             ret.append(item)
     return ret
+
+
+def flatten_comprehension(items):
+    '''
+    flatten solution from the book
+    '''
+    return [element
+            for sub_element in items
+                for element in sub_element]
+
+
+def pig_latin_file(path):
+    '''
+    convert contents of file to pig latin
+    '''
+    return ' '.join(ch2.pig_latin(word)
+                for line in open(path, 'r')
+                for word in line.split())
+
+
+def flip_dict(dic):
+    '''
+    flip keys and values in dict
+    '''
+    return {val: key
+                for key, val in dic.items()}
+
+
+def transform_values(func, dic):
+    '''
+    transform values of dict using func
+    '''
+    return {key: func(val)
+                for key, val in dic.items()}
+
+
+def get_sv(path):
+    '''
+    return set containing all words from dictionary
+    file that contain all 5 vowels (a, e, i , o, u)
+    '''
+    return {word.strip()
+                for word in open(path, 'r')
+                if 'a' in word.lower() and
+                    'e' in word.lower() and
+                    'i' in word.lower() and
+                    'o' in word.lower() and
+                    'u' in word.lower()}
+
+
+def gematria():
+    '''
+    returns all lowercase letters and their corresponding
+    "gematria" values
+    '''
+    return {chr(val): val - 96
+                for val in range(97, 123)}
+
+
+def gematria_for(word):
+    '''
+    returns gematria score for a word
+    '''
+    return sum(gematria()[letter]
+                    for letter in word)
--- src/ch7_test.py
+++ src/ch7_test.py
@@ -5,6 +5,7 @@ Chapter 7 Tests
 '''
 
 import ch7
+import hashlib
 
 
 def test_join_numbers():
@@ -51,9 +52,9 @@ def test_sum_numbers():
         assert got == case['expected'], f'{got} != {case["expected"]}'
 
 
-def test_flatten():
+def test_flatten_recursive():
     '''
-    test ch7.flatten()
+    test ch7.flatten_recursive()
     '''
     testcases = [
         {
@@ -71,5 +72,154 @@ def test_flatten():
     ]
     
     for case in testcases:
-        got = ch7.flatten(case['input'])
+        got = ch7.flatten_recursive(case['input'])
+        assert got == case['expected'], f'{got} != {case["expected"]}'
+
+
+def test_flatten_comprehension():
+    '''
+    test ch7.flatten_comprehension()
+    '''
+    testcases = [
+        {
+            'input': [[1, 2], [3, 4]],
+            'expected': [1, 2, 3, 4],
+        },
+    ]
+    for case in testcases:
+        got = ch7.flatten_comprehension(case['input'])
+        assert got == case['expected'], f'{got} !+ {case["expected"]}'
+
+
+def test_pig_latin_file():
+    '''
+    test ch7.pig_latin_file()
+    '''
+    testcases = [
+        {
+            'input': 'testdata/time_machine_chapter1.txt',
+            'checksum': '581f9205bee054da0b1f9e9ddfce6720679a8932900c19637ef6a64f21bb9a69',
+        },
+        {
+            'input': 'testdata/manifesto.txt',
+            'checksum': 'eaea19b875fc2cf523404759e8300615ed574ab9a5d05e65bc4a9508f40dd653',
+        }
+    ]
+
+    for case in testcases:
+        gotstr = ch7.pig_latin_file(case['input'])
+        gotbyt = bytes(gotstr, 'utf-8')
+        got = hashlib.sha256(gotbyt).hexdigest()
+        assert got == case['checksum'], f'pig_latin_file({case["input"]}) is unexpected checksum {got}'
+
+
+def test_flip_dict():
+    '''
+    test ch7.flip_dict()
+    '''
+    testcases = [
+        {
+            'input': {'a': 1, 'b': 2, 'c': 3, 'd': 4,},
+            'expected': {1: 'a', 2: 'b', 3: 'c', 4: 'd',},
+        }
+    ]
+
+    for case in testcases:
+        got = ch7.flip_dict(case['input'])
+        assert got == case['expected'], f'{got} != {case["expected"]}'
+
+
+def test_transform_values():
+    '''
+    test ch7.transform_values()
+    '''
+    testcases = [
+        {
+            'input': {'a': 1, 'b': 2, 'c': 3, 'd': 4,},
+            'func': lambda x: x*x,
+            'expected': {'a': 1, 'b': 4, 'c': 9, 'd': 16,},
+        },
+        {
+            'input': {'a': 1, 'b': 2, 'c': 3, 'd': 4,},
+            'func': lambda x: x+x,
+            'expected': {'a': 2, 'b': 4, 'c': 6, 'd': 8,},
+        },
+        {
+            'input': {'a': 2, 'b': 3, 'c': 4, 'd': 5,},
+            'func': lambda x: f'{x} bottles of beer',
+            'expected': {
+                'a': '2 bottles of beer',
+                'b': '3 bottles of beer',
+                'c': '4 bottles of beer',
+                'd': '5 bottles of beer',
+            },
+        },
+    ]
+
+    for case in testcases:
+        got = ch7.transform_values(case['func'], case['input'])
+        assert got == case['expected'], f'{got} != {case["expected"]}'
+
+
+def test_get_sv():
+    '''
+    test ch7.get_sv()
+    '''
+    testcases = [
+        {
+            'file_name': 'testdata/dict/words.txt',
+            'expected_count': 6004,
+        },
+        {
+            'file_name': 'testdata/dict/words_small.txt',
+            'expected_count': 2,
+        },
+    ]
+    for case in testcases:
+        got = ch7.get_sv(case['file_name'])
+        assert len(got) == case['expected_count'], f'{got} != {case["expected_count"]}'
+
+
+def test_gematria():
+    '''
+    test ch7.gematria()
+    '''
+    expected = {
+        'a': 1, 'b': 2, 'c': 3, 'd': 4,
+        'e': 5, 'f': 6, 'g': 7, 'h': 8,
+        'i': 9, 'j': 10, 'k': 11, 'l': 12,
+        'm': 13, 'n': 14, 'o': 15, 'p': 16,
+        'q': 17, 'r': 18, 's': 19, 't': 20,
+        'u': 21, 'v': 22, 'w': 23, 'x': 24,
+        'y': 25, 'z': 26,
+    }
+    got = ch7.gematria()
+    assert got == expected, f'{got} !+ {expected}'
+
+
+def test_gematria_for():
+    '''
+    test ch7.gematria_for()
+    '''
+    testcases = [
+        {
+            'input': 'a',
+            'expected': 1,
+        },
+        {
+            'input': 'abc',
+            'expected': 6,
+        },
+        {
+            'input': '',
+            'expected': 0,
+        },
+        {
+            'input': 'python',
+            'expected': 98,
+        },
+    ]
+    
+    for case in testcases:
+        got = ch7.gematria_for(case['input'])
         assert got == case['expected'], f'{got} != {case["expected"]}'
